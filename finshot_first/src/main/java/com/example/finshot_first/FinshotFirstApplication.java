2772caf48f48a61e5d867d503f71b74b02d17089
CHOIALTH <dnjswns4545@gmail.com>
2023-05-08 23:53:08 +0900

    [feat] finshot_first initial commit

---
+++ finshot_first/src/main/java/com/example/finshot_first/FinshotFirstApplication.java
@@ -0,0 +1,13 @@
+package com.example.finshot_first;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinshotFirstApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FinshotFirstApplication.class, args);
+    }
+
+}
