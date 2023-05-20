e25464f10224029096b6eb11ce946858c2210f21
CHOIALTH <dnjswns4545@gmail.com>
2023-05-20 23:17:44 +0900

    [feat] add RestController

--- test_gradle/src/main/java/com/example/test_gradle/controller/MainController.java
+++ test_gradle/src/main/java/com/example/test_gradle/controller/MainController.java
@@ -1,9 +1,9 @@
 package com.example.test_gradle.controller;
 
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+@RestController
 public class MainController {
     @RequestMapping("/")
     public String home(){
