88ced021911ad439f56b782d39e5b00303a9931c
CHOIALTH <dnjswns4545@gmail.com>
2023-05-30 03:09:13 +0900

    [feat] Add dependency injection of Spring HATEOAS refer to Spring.io docs

--- test_gradle/src/main/java/com/example/test_gradle/controller/EmployeeController.java
+++ test_gradle/src/main/java/com/example/test_gradle/controller/EmployeeController.java
@@ -3,9 +3,13 @@ package com.example.test_gradle.controller;
 import com.example.test_gradle.entity.Employee;
 import com.example.test_gradle.exception.EmployeeNotFoundException;
 import com.example.test_gradle.repository.EmployeeRepository;
+import org.springframework.hateoas.EntityModel;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn;
 
 @RestController
 public class EmployeeController {
@@ -29,10 +33,24 @@ public class EmployeeController {
     }
     // Single item
 
+//    @GetMapping("/employees/{id}")
+//    Employee one(@PathVariable Long id){
+//        return employeeRepository.findById(id)
+//                .orElseThrow(()-> new EmployeeNotFoundException(id));
+//    }
     @GetMapping("/employees/{id}")
-    Employee one(@PathVariable Long id){
-        return employeeRepository.findById(id)
-                .orElseThrow(()-> new EmployeeNotFoundException(id));
+    EntityModel<Employee> one(@PathVariable Long id){
+
+        Employee employee = employeeRepository.findById(id)
+                .orElseThrow(()->new EmployeeNotFoundException(id));
+
+        return EntityModel.of(employee,
+                // asks that Spring HATEOAS build a link to the EmployeeController's one() method, and flag it as a self link.
+                linkTo(methodOn(EmployeeController.class).one(id)).withSelfRel(),
+                // asks Spring HATEOAS to build a link to the aggregate root, all(), and call it "employees".
+                linkTo(methodOn(EmployeeController.class).all()).withRel("employees")
+                // build a link : One of Spring HATEOAS's core types in Link. It includes a URI and a rel(relation).
+        );
     }
 
     @PutMapping("/employees/{id}")
